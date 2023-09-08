5da3ead29f17d639f4337241a1fd7ff413eed719
Jerry Batorski <jarekb@gmail.com>
2023-09-08 18:37:30 -0500

    Add SubvertADown support (#5)
    * Refactor settings panel to make it composable
    
    * Add tabs
    
    * Add SubvertADown tab and saving of data
    
    * Added SubvertADown parsing
    
    * Update player info to show both boris chen and subvertadown
    
    * Fix DST support from subertadown

--- fuse.js
+++ fuse.js
@@ -5,14 +5,14 @@
         showSettingsBtn: 'fuseShowSettings',
         settingPanel: {
             name: 'fuseSettingsPanel',
-            borischen: 'fuseSettingsPanel_borischen'
+            tabs: 'fuseSettingsPanel__tabs',
+            borisChen: 'fuseSettingsPanel__tabs__borisChen',
+            subvertADown: 'fuseSettingsPanel__tabs__subvertADown'
         },
         localStorage: 'fuseStorage'
     }
 
-    const runUpdateBtn = makeButton(selectors.runUpdateBtn, 'Update', 150);
-
-    runUpdateBtn.addEventListener('click', updatePlayerInfo);
+    makePageButton(selectors.runUpdateBtn, 'Update', 150, updatePlayerInfo);
 
     function updatePlayerInfo() {
         const spans = document.querySelectorAll(`.${selectors.playerInfo}`);
@@ -21,13 +21,17 @@
             span.remove();
         });
 
-        const players = getStoredData().data.borischen.parsed;
+        const data = getStoredData().data;
+        const borisChen = data.borisChen.parsed
+        const subvertADown = data.subvertADown.parsed
 
         document.querySelectorAll('.player-column__bio .AnchorLink.link').forEach(playerNameEl => {
             const name = playerNameEl.innerText;
-            const playerTier = players[name];
+            const borisChenTier = borisChen[name];
+            const subvertADownValue = subvertADown[name];
+            const info = [borisChenTier, subvertADownValue].filter(val => val);
 
-            if (!playerTier || !name) {
+            if (!info.length || !name) {
                 return
             }
 
@@ -42,7 +46,7 @@
                     span.className = selectors.playerInfo;
                     span.style.marginRight = '2px';
                     span.style.fontWeight = '900';
-                    span.textContent = playerTier;
+                    span.textContent = `${info.join('/')}`;
 
                     playerPositionElem.insertBefore(span, playerPositionElem.firstChild);
                 }
@@ -50,9 +54,7 @@
         });
     }
 
-    const configure = makeButton(selectors.showSettingsBtn, '⚙', 175);
-
-    configure.addEventListener('click', editSettings);
+    makePageButton(selectors.showSettingsBtn, '⚙', 175, editSettings);
 
     function editSettings() {
         if (document.getElementById(selectors.settingPanel.name)) {
@@ -61,75 +63,280 @@
             return;
         }
 
-        const section = document.createElement('div');
+        let settingsPanel = createMainSettingsPanel();
+        const savedData = getStoredData().data
 
-        section.setAttribute('id', selectors.settingPanel.name);
-        section.style.position = 'absolute';
-        section.style.top = '200px';
-        section.style.right = '0';
-        section.style.backgroundColor = '#f9f9f9';
-        section.style.width = '200px';
+        const borisChenTab = createBorisChenTab(savedData.borisChen)
 
-        section.style.padding = '15px';
-        section.style.border = '1px solid #ccc';
-        section.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.1)';
+        const toggleBorisChenTab = makeButton('BorisChen', () => {
+            toggleTabs(borisChenTab.id)
+        });
 
+        const subvertADownTab = createSubvertADownTab(savedData.subvertADown);
+        const toggleSubvertADownTab = makeButton('SubvertADown', () => {
+            toggleTabs(subvertADownTab.id)
+        });
+        settingsPanel.appendChild(toggleBorisChenTab);
+        settingsPanel.appendChild(toggleSubvertADownTab);
+        settingsPanel.appendChild(borisChenTab);
+        settingsPanel.appendChild(subvertADownTab);
 
-        const helpText = document.createElement('p');
+        settingsPanel.appendChild(document.createElement('br'));
 
-        helpText.textContent = 'To get the tier data from www.borischen.co for your league\'s point values and paste the raw tier info into the below text areas.';
-        section.appendChild(helpText);
+        const saveBtn = makeButton('Save', () => {
+            let state = getStoredData();
 
-        const positions = ['QB', 'RB', 'WR', 'TE', 'DST', 'K'];
-        const savedData = getStoredData().data.borischen.raw;
-
-        for (const position of positions) {
-            const label = document.createElement('label');
-
-            label.textContent = position;
-
-            const textarea = document.createElement('textarea');
-
-            textarea.setAttribute('id', `${selectors.settingPanel.borischen}_${position}`);
-            if (savedData[position]) {
-                textarea.value = savedData[position];
-            }
-
-            section.appendChild(label);
-            section.appendChild(document.createElement('br'));
-
-            section.appendChild(textarea);
-            section.appendChild(document.createElement('br'));
-
-        }
-
-        const saveBtn = document.createElement('button');
-
-        saveBtn.textContent = 'Save';
-        saveBtn.addEventListener('click', () => {
-            let state = {
-                data: {
-                    borischen: {
-                        raw: getBorischenFormData(),
-                        parsed: {}
-                    }
-                }
-            }
-
-            state.data.borischen.parsed = parseBorischenRawData(state.data.borischen.raw);
+            state.data.borisChen.raw = getBorischenFormData();
+            state.data.borisChen.parsed = parseBorischenRawData(state.data.borisChen.raw);
+            state.data.subvertADown.raw = getSubvertADownFormData();
+            state.data.subvertADown.parsed = parseSubvertADownFormRawData(state.data.subvertADown.raw);
             saveToLocalStorage(state);
             hideSettings();
             updatePlayerInfo();
         });
-        section.appendChild(saveBtn);
 
-        const hideBtn = document.createElement('button');
+        settingsPanel.appendChild(saveBtn);
+        settingsPanel.appendChild(makeButton('Hide', hideSettings));
 
-        hideBtn.textContent = 'Hide';
-        hideBtn.addEventListener('click', hideSettings);
-        section.appendChild(hideBtn);
+        document.body.insertBefore(settingsPanel, document.getElementById(selectors.showSettingsBtn).nextSibling);
+        toggleTabs(borisChenTab.id);
 
-        document.body.insertBefore(section, document.getElementById(selectors.showSettingsBtn).nextSibling);
+        function createMainSettingsPanel() {
+            const settingsPanel = document.createElement('div');
+
+            settingsPanel.setAttribute('id', selectors.settingPanel.name);
+            settingsPanel.style.position = 'absolute';
+            settingsPanel.style.top = '200px';
+            settingsPanel.style.right = '0';
+            settingsPanel.style.backgroundColor = '#f9f9f9';
+            settingsPanel.style.width = '400px';
+            settingsPanel.style.padding = '10px';
+
+            settingsPanel.style.padding = '15px';
+            settingsPanel.style.border = '1px solid #ccc';
+            settingsPanel.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.1)';
+
+            return settingsPanel
+        }
+
+        function createBorisChenTab(savedData) {
+            const tab = document.createElement('div');
+            tab.id = selectors.settingPanel.borisChen;
+            tab.className = selectors.settingPanel.tabs;
+            tab.appendChild(document.createElement('br'));
+            const helpText = document.createElement('p');
+
+            helpText.textContent = 'To get the tier data from www.borisChen.co for your league\'s point values and paste the raw tier info into the below text areas.';
+            tab.appendChild(helpText);
+            tab.appendChild(document.createElement('br'));
+
+            const positions = ['QB', 'RB', 'WR', 'TE', 'DST', 'K'];
+
+            for (const position of positions) {
+                const label = document.createElement('label');
+
+                label.textContent = position;
+
+                const textarea = document.createElement('textarea');
+                textarea.style.width = '350px';
+
+                textarea.setAttribute('id', `${selectors.settingPanel.borisChen}_${position}`);
+                if (savedData.raw[position]) {
+                    textarea.value = savedData.raw[position];
+                }
+
+                tab.appendChild(label);
+                tab.appendChild(document.createElement('br'));
+
+                tab.appendChild(textarea);
+                tab.appendChild(document.createElement('br'));
+            }
+
+            return tab;
+        }
+
+        function getBorischenFormData() {
+            const data = {};
+
+            const positions = ['QB', 'RB', 'WR', 'TE', 'DST', 'K'];
+
+            for (const position of positions) {
+                data[position] = document.getElementById(`${selectors.settingPanel.borisChen}_${position}`).value;
+            }
+
+            return data;
+        }
+
+        function parseBorischenRawData(rawTierData) {
+            const players = {};
+
+            parseTierInfo(rawTierData.QB, players);
+            parseTierInfo(rawTierData.RB, players);
+            parseTierInfo(rawTierData.WR, players);
+            parseTierInfo(rawTierData.TE, players);
+            parseTierInfo(removeCityFromDST(rawTierData.DST), players);
+            parseTierInfo(rawTierData.K, players);
+
+            return players;
+
+            function parseTierInfo(raw, playerDictionary) {
+                if (!raw) {
+                    return
+                }
+
+                const tiers = raw.split('\n');
+
+                tiers.forEach(tierRow => {
+                    const row = tierRow.split(': ');
+                    const tier = row[0].replace('Tier ', '');
+                    const players = row[1];
+
+                    players?.split(', ').forEach((player, index) => {
+                        playerDictionary[player] = `${tier}.${index + 1}`;
+                    });
+                });
+
+
+                return playerDictionary;
+            }
+
+            function removeCityFromDST(raw) {
+                if (!raw) {
+                    return;
+                }
+
+                const tiers = raw.split('\n');
+                const output = [];
+
+                tiers.forEach(tierRow => {
+                    const row = tierRow.split(': ');
+                    const tier = row[0];
+                    const teams = row[1];
+                    let rowOutput = `${tier}: `;
+                    let isFirst = true;
+
+                    teams?.split(', ').forEach(team => {
+                        const name = team.split(' ').pop();
+
+                        if (isFirst) {
+                            rowOutput += `${name} D/ST`;
+                            isFirst = false;
+                        } else {
+                            rowOutput += `, ${name} D/ST`;
+                        }
+                    });
+
+                    output.push(rowOutput);
+                });
+
+                return output.join('\n');
+            }
+        }
+
+        function createSubvertADownTab(savedData) {
+            const tab = document.createElement('div');
+            tab.id = selectors.settingPanel.subvertADown;
+            tab.className = selectors.settingPanel.tabs;
+            tab.appendChild(document.createElement('br'));
+            const helpText = document.createElement('p');
+
+            helpText.textContent = 'Copy data from https://subvertadown.com and paste the raw tier info into the below text areas.';
+            tab.appendChild(helpText);
+            tab.appendChild(document.createElement('br'));
+
+            const positions = ['DST', 'QB', 'K'];
+
+            for (const position of positions) {
+                const label = document.createElement('label');
+
+                label.textContent = position;
+
+                const textarea = document.createElement('textarea');
+                textarea.style.width = '350px';
+
+                textarea.setAttribute('id', `${selectors.settingPanel.subvertADown}_${position}`);
+                if (savedData?.raw[position]) {
+                    textarea.value = savedData.raw[position];
+                }
+
+                tab.appendChild(label);
+                tab.appendChild(document.createElement('br'));
+
+                tab.appendChild(textarea);
+                tab.appendChild(document.createElement('br'));
+            }
+
+            return tab;
+        }
+
+        function getSubvertADownFormData() {
+            const data = {};
+
+            const positions = ['QB', 'DST', 'K'];
+
+            for (const position of positions) {
+                data[position] = document.getElementById(`${selectors.settingPanel.subvertADown}_${position}`).value;
+            }
+
+            return data;
+        }
+
+        function parseSubvertADownFormRawData(rawData) {
+            const players = {};
+
+            processData(rawData.DST, players, true);
+            processData(rawData.QB, players);
+            processData(rawData.K, players);
+
+            return players;
+
+            function processData(input, players, isDST) {
+                const lines = input.trim().split('\n');
+
+                let player = '';
+                lines.forEach(line => {
+                    // skip blank lines
+                    if (!line.trim()) {
+                        return;
+                    }
+
+                    if (!player) {
+                        player = line.split('|')[0].trim();
+
+                        if(isDST){
+                            player = `${player} D/ST`;
+                        }
+                    } else {
+                        value = line.trim();
+                        players[player] = value;
+
+                        // reset for next iteration
+                        player = ''
+                    }
+                })
+
+
+                return players;
+            }
+        }
+
+        function hideAllTabs() {
+            const tabs = document.querySelectorAll(`.${selectors.settingPanel.tabs}`);
+
+            tabs.forEach(tab => {
+                tab.style.display = 'none';
+            });
+        };
+
+        function showTab(tabId) {
+            var element = document.getElementById(tabId);
+            element.style.display = 'block';
+        }
+
+        function toggleTabs(tabId) {
+            hideAllTabs();
+            showTab(tabId);
+        }
     }
 
     function hideSettings() {
@@ -138,18 +345,26 @@
 
     function getStoredData() {
         const storedData = localStorage.getItem(selectors.localStorage);
-        const parsedData = JSON.parse(storedData) || {
+        const parsedData = JSON.parse(storedData) || {};
+
+        let defaults = {
             data: {
-                borischen: {
+                borisChen: {
+                    raw: {},
+                    parsed: {}
+                },
+                subvertADown: {
                     raw: {},
                     parsed: {}
                 }
             }
         };
 
-        console.log(parsedData)
+        const result = mergeDeep(defaults, parsedData)
 
-        return parsedData;
+        console.log(result)
+
+        return result;
     }
 
     function saveToLocalStorage(data) {
@@ -157,95 +372,41 @@
         console.log(data)
     }
 
-    function getBorischenFormData() {
-        const data = {};
-
-        const positions = ['QB', 'RB', 'WR', 'TE', 'DST', 'K'];
-
-        for (const position of positions) {
-            data[position] = document.getElementById(`${selectors.settingPanel.borischen}_${position}`).value;
-        }
-
-        return data;
+    function isObject(item) {
+        return (item && typeof item === 'object' && !Array.isArray(item));
     }
 
-    function parseBorischenRawData(rawTierData) {
-        const players = {};
+    // copy pasta from https://stackoverflow.com/a/34749873/276681
+    // didn't want to pull in lodash just yet
+    function mergeDeep(target, ...sources) {
+        if (!sources.length) return target;
+        const source = sources.shift();
 
-        parseTierInfo(rawTierData.QB, players);
-        parseTierInfo(rawTierData.RB, players);
-        parseTierInfo(rawTierData.WR, players);
-        parseTierInfo(rawTierData.TE, players);
-        parseTierInfo(removeCityFromDST(rawTierData.DST), players);
-        parseTierInfo(rawTierData.K, players);
-
-        return players;
-
-        function parseTierInfo(raw, playerDictionary) {
-            if (!raw) {
-                return
+        if (isObject(target) && isObject(source)) {
+            for (const key in source) {
+                if (isObject(source[key])) {
+                    if (!target[key]) Object.assign(target, { [key]: {} });
+                    mergeDeep(target[key], source[key]);
+                } else {
+                    Object.assign(target, { [key]: source[key] });
+                }
             }
-
-            const tiers = raw.split('\n');
-
-            tiers.forEach(tierRow => {
-                const row = tierRow.split(': ');
-                const tier = row[0].replace('Tier ', '');
-                const players = row[1];
-
-                players?.split(', ').forEach((player, index) => {
-                    playerDictionary[player] = `${tier}.${index + 1}`;
-                });
-            });
-
-
-            return playerDictionary;
         }
 
-        function removeCityFromDST(raw) {
-            if (!raw) {
-                return;
-            }
-
-            const tiers = raw.split('\n');
-            const output = [];
-
-            tiers.forEach(tierRow => {
-                const row = tierRow.split(': ');
-                const tier = row[0];
-                const teams = row[1];
-                let rowOutput = `${tier}: `;
-                let isFirst = true;
-
-                teams?.split(', ').forEach(team => {
-                    const name = team.split(' ').pop();
-
-                    if (isFirst) {
-                        rowOutput += `${name} D/ST`;
-                        isFirst = false;
-                    } else {
-                        rowOutput += `, ${name} D/ST`;
-                    }
-                });
-
-                output.push(rowOutput);
-            });
-
-            return output.join('\n');
-        }
+        return mergeDeep(target, ...sources);
     }
 
-    function makeButton(id, text, offset) {
+
+    function makePageButton(id, text, offset, onClick) {
         const existingBtn = document.getElementById(id);
 
         if (document.contains(existingBtn)) {
             existingBtn.remove();
         }
 
-        const button = document.createElement('button');
-
+        const button = makeButton(text, onClick);
         button.id = id;
-        button.innerHTML = text;
+
         button.style.position = 'absolute';
         button.style.top = `${offset}px`;
         button.style.right = 0;
@@ -255,6 +416,15 @@
         const body = document.getElementsByTagName('body')[0];
 
         body.appendChild(button);
+
+        return button;
+    }
+
+    function makeButton(text, onClick) {
+        const button = document.createElement('button');
+
+        button.innerHTML = text;
+        button.addEventListener('click', onClick);
 
         return button;
     }
